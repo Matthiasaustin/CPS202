109ddd3b235fabe66977375ba210da54f4e09f47
Matthiasaustin <Matthiasaustin@gmail.com>
2013-03-19 15:57:11 -0300

    update to the important date driver
    trying to read in dates, not sure how yet.

--- src/lab7/ImDateDriver.java
+++ src/lab7/ImDateDriver.java
@@ -5,16 +5,19 @@ public class ImDateDriver
 {
   public static void main(String[] args)
   {
-    ImportantDates[] myDates;
-    myDates = new ImportantDates[20];
+    ImportantDate[] myDates;
+    myDates = new ImportantDate[20];
     Scanner inFile = new Scanner(new FileReader("myDates"));
     Date aDate;
+    
+    
     ImportantDate myDate;
-    while (inFile.hadNextLine())
+    while (inFile.hasNextLine())
     {
-      /* TO BE FILLED IN: Exercise 3 */
-      adate = new ImportantDate();
-      myDates[1]=adate;
+    aDate = new Date(inFile.nextInt(),inFile.nextInt(),inFile.nextInt());
+    
+      aDate = new ImportantDate();
+      myDates[1]=aDate;
     }
   }
 }
