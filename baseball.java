0bd211f4daaddb37aaceead649f21b94ed93a461
Matthias Austin <Matthiasaustin@gmail.com>
2013-03-23 17:08:51 -0400

    New files for CPS202 HW3

---
+++ baseball.java
@@ -0,0 +1,83 @@
+ 
+ 
+/*
+*Array to hold the data recieved or calculated in app
+*
+*col 0 holds player id
+*col 1 holds number of hits (summed)
+*col 2 holds number of walks summed
+*col 3 holds number of outs summed
+*col 4 holds number of at bats the player had
+*col 5 holds the players batting average
+*/
+int [][] baseball;					
+baseball = new int [20][4];
+
+//holds the contents of the line as read in
+int [] line;
+line = new int [4];
+
+
+ public static void main (String [] args)
+ {
+
+//read in line from file into line array
+
+//assign values to baseball array, summing values as values are read in 
+
+//calculate at bats
+
+//calculatee batting average
+
+//format results
+
+//print results for all players
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
