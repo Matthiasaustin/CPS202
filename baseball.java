af632f32376e24e3e31ee8b601e79f5b4bc4a075
Matthias Austin <Matthiasaustin@gmail.com>
2013-03-23 23:27:13 -0400

    running order, output funky
    need to work on the output, verify results

--- baseball.java
+++ baseball.java
@@ -2,53 +2,43 @@
  
 public class baseball
 {
-/*
-*Array to hold the data recieved or calculated in app
-*
-*col 0 holds player id
-*col 1 holds number of hits (summed)
-*col 2 holds number of walks summed
-*col 3 holds number of outs summed
-*col 4 holds number of at bats the player had
-*col 5 holds the players batting average
-*/
-int [][] baseball;					
-baseball = new int [20][4];
 
-//holds the contents of the line as read in
-int [] line;
-line = new int [4];
+	/*
+	 *Array to hold the data recieved or calculated in app
+	 *
+	 *col 0 holds player id
+	 *col 1 holds number of hits (summed)
+	 *col 2 holds number of walks summed
+	 *col 3 holds number of outs summed
+	 *col 4 holds number of at bats the player had
+	 *col 5 holds the players batting average
+	 */
+	static int [][] baseball = new int [20][5];
 
-Scanner inFile = new Scanner(new FileReader("//Volumes//Storage//Code//CPS202//playdata."));
+	//holds the contents of the line as read in
+	static int [] line = new int [4];
 
- public static void main (String [] args)
- {
+	
 
-while (inFile.hasNextLine())
-{
-//read in line from file into line array
-for(int index = 0; inFile.hasNextInt(); index++)
-{
-	line[index] = inFile.nextInt();
-}
-//assign values to baseball array, summing values as values are read in 
-baseball[0] = line[0];
-for(int index = 1; index < line.length; index++)
-{
-//	baseball [¡index-1][index]= line [index];
-baseball [line[0]][index - 1] = line [index];
-}
-//calculate at bats
-for (into index = 0; )
-//calculatee batting average
-
-//format results
-
-//print results for all players
-
-
-}
- }
+	public static void main (String [] args) throws FileNotFoundException
+	{
+		Scanner inFile = new Scanner(new FileReader("//Volumes//Storage//Code//CPS202//playdata"));
+		while (inFile.hasNextLine())
+		{
+			//read in line from file into line array
+			for(int index = 0; index < 4; index++)
+			{
+				line[index] = inFile.nextInt();
+			}
+			//assign values to baseball array, summing values as values are read in 
+			for(int index = 1; index <= 3; index++)
+			{
+				baseball [line[0]][index - 1] = baseball [line[0]][index - 1] + line [index];
+			}
+			//calculate at bats
+			baseball [line[0]][3] = baseball [line[0]][0] + baseball [line[0]][1] + baseball [line[0]][2];
+			//calculates batting average
+			baseball [line[0]][4] = (baseball [line[0]][0] / (baseball [line[0]][4] + 1))-1;
 			
 			
 			//format and print results
@@ -56,4 +46,10 @@ for (into index = 0; )
 				for(int c = 0; c < 20; c++ )
 				{
 					System.out.println ( baseball[c] + " " + baseball [c][4] + " " + baseball [c][1]);	
+				}
+		
+
+
+			}
+		}
 }
