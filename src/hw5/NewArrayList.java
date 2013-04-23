b75702be23914dffac02ab0a9993ffa935ba9445
Matthiasaustin <Matthiasaustin@gmail.com>
2013-04-23 17:14:33 -0300

    New ArrayList for #10 in HW5

--- src/hw5/NewArrayList.java
+++ src/hw5/NewArrayList.java
@@ -4,23 +4,24 @@ import java.util.*;
 
 public class NewArrayList extends ArrayList<Object>
 {
-			static int numItems;
-			static Comparable [] values;
-			public void add(Comparable item)
-			{
-				// Pre:  The list is not full 
-				// Post: item is in the list
-				
-				values [numItems] = (String) item;
-				numItems++;
-			}
+		
 	public static void main (String [] args)
 	{
-		ArrayList[] list1 = new ArrayList [100];
+		ArrayList <int> list1 = new ArrayList <int> (100);
 		System.out.println(list1.length);
-//		ArrayList [] list2;
-	//	System.out.println(list2.length);
-		
-		
+		ArrayList<int> list2 = new ArrayList<int>();
+		System.out.println(list2.length);
+		list1.add(7);
+		Iterator listIterator = list1.iterator();
+		while (listIterator.hasNext())
+		{
+			System.out.println(list1.next());
+		}
+		ArrayList <String> stringList = new ArrayList <String>(20)
+		Iterator stringIterator = stringList.iterator();
+		while (stringIterator.hasNext())
+		{
+			System.out.println(stringList.next());
+		}
 	}
 }
